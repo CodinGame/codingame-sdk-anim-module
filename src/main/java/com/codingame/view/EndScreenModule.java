4035fa488536c5ee2186883ad8684a7730785a35
Thomas Foucault <thomas@codingame.com>
2018-05-17 11:42:12 +0200

    add end screen and tooltip modules

---
+++ src/main/java/com/codingame/view/EndScreenModule.java
@@ -0,0 +1,41 @@
+package com.codingame.view;
+
+import com.codingame.gameengine.core.AbstractPlayer;
+import com.codingame.gameengine.core.GameManager;
+import com.codingame.gameengine.core.Module;
+import com.google.inject.Inject;
+
+public class EndScreenModule implements Module {
+
+    private GameManager<AbstractPlayer> gameManager;
+    private int[] scores;
+
+    @Inject
+    EndScreenModule(GameManager<AbstractPlayer> gameManager) {
+        this.gameManager = gameManager;
+        gameManager.registerModule(this);
+    }
+
+    public void setScores(int[] scores) {
+        this.scores = scores;
+    }
+
+    @Override
+    public final void onGameInit() {
+    }
+
+    private void sendFrameData() {
+        gameManager.setViewData("endScreen", scores);
+    }
+
+    @Override
+    public final void onAfterGameTurn() {
+        sendFrameData();
+    }
+
+    @Override
+    public final void onAfterOnEnd() {
+        sendFrameData();
+    }
+
+}
--- src/main/resources/view/config.js
+++ src/main/resources/view/config.js
@@ -2,5 +2,7 @@ import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
 
 // List of viewer modules that you want to use in your game
 export const modules = [
-	GraphicEntityModule
+	GraphicEntityModule,
+	TooltipModule,
+	EndScreenModule
 ];
